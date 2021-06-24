56ce3df7ba1948ee0e4fb187222b2354a78758d0
2smirtan2 <tan.kov@hotmail.com>
2021-06-24 18:44:20 +0300

    mv logic of program in Cli.php & add namespace

--- src/Cli.php
+++ src/Cli.php
@@ -2,3 +2,12 @@
 
 namespace Brain\Games\Cli;
 
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt('May I have you name?');
+    line("Hello, %s!", $name);
+}
