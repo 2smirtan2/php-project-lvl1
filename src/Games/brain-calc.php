b49ef070dbb1781441b8ab3b7eb6e3c31fe51160
2smirtan2 <tan.kov@hotmail.com>
2021-06-28 16:06:28 +0300

    fix hexlet-check

--- src/Games/brain-calc.php
+++ src/Games/brain-calc.php
@@ -5,7 +5,7 @@ namespace Brain\Games\Calc;
 use Brain\Games\Engine;
 use Brain\Games\Helper;
 
-function run(): string
+function run()
 {
     $description = 'What is the result of the expression?';
 
