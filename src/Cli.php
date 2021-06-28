142983c3ddf7ef24f1655c59c75b121b77f2fba3
2smirtan2 <tan.kov@hotmail.com>
2021-06-28 15:06:24 +0300

    fix hexlet-check

--- src/Cli.php
+++ src/Cli.php
@@ -8,6 +8,6 @@ use function cli\prompt;
 function run()
 {
     line("Welcome to the Brain Games!");
-    $name = prompt('May I have you name?');
+    $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
